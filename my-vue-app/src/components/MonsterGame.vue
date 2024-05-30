7ad677adc34509f711cd73e54722b36f0bdb7ea9
Anosha Sohail <86046973+AnoshaSohail@users.noreply.github.com>
2024-05-30 05:26:26 +0000

    code setup

--- my-vue-app/src/components/MonsterGame.vue
+++ my-vue-app/src/components/MonsterGame.vue
@@ -1,32 +1,47 @@
 <template>
+  <div>
     <header>
       <h1>Monster Slayer</h1>
-    </header>
-    <div id="game">
+  </header>
+  <div id="game">
       <section id="monster" class="container">
-        <h2>Monster Health</h2>
-        <div class="healthbar">
-          <div class="healthbar__value"></div>
-        </div>
+          <h2>Monster Health</h2>
+          <div class="healthbar">
+              <div class="healthbar__value"></div>
+          </div>
       </section>
       <section id="player" class="container">
-        <h2>Your Health</h2>
-        <div class="healthbar">
-          <div class="healthbar__value"></div>
-        </div>
+          <h2>Your Health</h2>
+          <div class="healthbar">
+              <div class="healthbar__value"></div>
+          </div>
       </section>
       <section id="controls">
-        <button>ATTACK</button>
-        <button>SPECIAL ATTACK</button>
-        <button>HEAL</button>
-        <button>SURRENDER</button>
+          <button>ATTACK</button>
+          <button>SPECIAL ATTACK</button>
+          <button>HEAL</button>
+          <button>SURRENDER</button>
       </section>
       <section id="log" class="container">
-        <h2>Battle Log</h2>
-        <ul></ul>
+          <h2>Battle Log</h2>
+          <ul></ul>
       </section>
-    </div>
+  </div>
+  </div>
 </template>
 <script>
+export default {
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        }
+    },
+    methods: {
+        attack() {
 
+        },
+
+    }
+}
 </script>
