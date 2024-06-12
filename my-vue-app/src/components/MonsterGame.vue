3affd224e86d23db9b09018025bc6d72ba19e8eb
Anosha Sohail <86046973+AnoshaSohail@users.noreply.github.com>
2024-06-12 07:01:30 +0000

    implemented special attack

--- my-vue-app/src/components/MonsterGame.vue
+++ my-vue-app/src/components/MonsterGame.vue
@@ -7,18 +7,18 @@
       <section id="monster" class="container">
           <h2>Monster Health</h2>
           <div class="healthbar">
-              <div class="healthbar__value"></div>
+              <div class="healthbar__value" :style="monsterBarStyle"></div>
           </div>
       </section>
       <section id="player" class="container">
           <h2>Your Health</h2>
           <div class="healthbar">
-              <div class="healthbar__value"></div>
+              <div class="healthbar__value" :style="playerBarStyle"></div>
           </div>
       </section>
       <section id="controls">
-          <button @click="attackMonster">ATTACK</button>
-          <button>SPECIAL ATTACK</button>
+          <button @click="attackMonster" >ATTACK</button>
+          <button @click="specialAttack" :disabled="canUseSA">SPECIAL ATTACK</button>
           <button>HEAL</button>
           <button>SURRENDER</button>
       </section>
@@ -34,8 +34,20 @@ export default {
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         }
+    },
+    computed:{
+        playerBarStyle(){
+            return { width: this.playerHealth + '%' }
+        },
+        monsterBarStyle(){
+            return { width: this.monsterHealth + '%' }
+        }, 
+     canUseSA(){
+        return this.currentRound % 3 !== 0
+     }
     },
     methods: {
         getRandomVal(min,max){
@@ -45,11 +57,18 @@ export default {
             //we'll attack the monster and reduce its health with a random number for example value between 5 and 12 so
            const attackValue = this.getRandomVal(5,12);
            this.monsterHealth -= attackValue;
-           this.attackPlayer()
+           this.attackPlayer();
+           this.currentRound++
         },
         attackPlayer(){
             const attackValue  = this.getRandomVal(8,15);
             this.playerHealth -= attackValue
+        },
+        specialAttack(){
+            this.currentRound++
+            const attackValue  = this.getRandomVal(10,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 }
